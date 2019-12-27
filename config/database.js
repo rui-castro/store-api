ba83d9156b38e7c60a1bfd33656b376f3e12ca55
Rui Castro <rui.castro@gmail.com>
2019-12-27 02:51:23 +0000

    adds product model and seeds

--- config/database.js
+++ config/database.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 module.exports = {
     development: {
         dialect: 'sqlite',
-        storage: 'development.sqlite'
+        storage: __dirname + '/../database/development.sqlite',
     },
     test: {
         dialect: 'sqlite',
-        storage: 'test.sqlite'
+        storage: __dirname + '/../database/test.sqlite',
     },
     production: {
         dialect: process.env.DB_DIALECT || 'sqlite',
-        storage: 'production.sqlite',
+        storage: __dirname + '/../database/production.sqlite',
         host: process.env.DB_HOSTNAME,
         database: process.env.DB_NAME,
         username: process.env.DB_USERNAME,
---
+++ database/migrations/20191227022514-create-product.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.NUMBER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Products');
+  }
+};
