509382b3e10a672e3d95b05dc52cccc843907bed
Rui Castro <rui.castro@gmail.com>
2020-01-03 01:37:57 +0000

    creates endpoint /products to list all products fixes product model definition

--- app/models/product.js
+++ app/models/product.js
@@ -1,36 +1,22 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
 
-    const modelAttributes = {
+    const Product = sequelize.define('Product', {
         name: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false,
             len: [1, 250]
         },
         price: {
-            type: Sequelize.FLOAT,
+            type: DataTypes.FLOAT,
             min: 0
         },
         description: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             len: [0,10000],
         }
-    };
-
-    // There's 2 ways of defining a model
-    // according to the manual (https://sequelize.org/master/manual/getting-started.html)
-
-    // Method 1
-    /*
-    class Product extends Sequelize.Model {}
-    Product.init(modelAttributes, {
-        sequelize,
-        modelName: 'product'
-    });
-    */
-
-    // Method 2
-    const Product = sequelize.define('product', modelAttributes, {});
+    }, {});
 
     Product.associate = function (models) {
         // associations can be defined here
---
+++ routes/products.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { Product } = require('../app/models');
+
+/* GET products listing. */
+router.get('/', function (req, res, next) {
+    Product.findAll().then(products => {
+        res.send(products);
+    });
+});
+
+module.exports = router;
