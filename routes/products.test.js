6a2c09d92a77c09bcbdbd4fadc0627e348851456
Rui Castro <rui.castro@gmail.com>
2020-01-06 11:44:32 +0000

    fixes products endpoint test

--- routes/products.test.js
+++ routes/products.test.js
@@ -4,10 +4,42 @@ const app = require('../app');
 describe('Products endpoints', () => {
 
     it('should get all products', async () => {
-        const res = await request(app)
-            .get('/products');
-        expect(res.statusCode).toEqual(200);
-        // expect(res.body).toHaveProperty('post');
-    })
+
+        return request(app)
+            .get('/products')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                expect(response.body.length).toBe(3);
+
+                testProduct(response.body[0], {
+                    id: 1,
+                    name: 'Lego Creator: Underwater Robot',
+                    price: 19.99,
+                    description: 'Lego Creator: Underwater Robot - 31090 - 7+'
+                });
+
+                testProduct(response.body[1], {
+                    id: 2,
+                    name: 'Lego Creator: Deep Sea Creatures',
+                    price: 14.99,
+                    description: 'Lego Creator: Deep Sea Creatures - 31088 - 7+'
+                });
+
+                testProduct(response.body[2], {
+                    id: 3,
+                    name: 'Lego Creator: Dune Buggy',
+                    price: 12.99,
+                    description: 'Lego Creator: Dune Buggy - 31087 - 6+'
+                });
+            });
+    });
+
+    function testProduct(received, expected) {
+        expect(received.id).toBe(expected.id);
+        expect(received.name).toBe(expected.name);
+        expect(received.price).toBe(expected.price);
+        expect(received.description).toBe(expected.description);
+    }
 
 });
